e0d84ae809fcd5a8f305edbe5445d3dfb1b67393
JE-Wardrop <3110drop@gmail.com>
2026-09-21 14:10:44 +1000

    tool paths for students. implmenet your tools based off file path

--- ai-services/mcp-server/tools.py
+++ ai-services/mcp-server/tools.py
@@ -2,17 +2,55 @@ import json
 import sqlite3
 from pathlib import Path
 
+
+# Will have to be based off each student's database
+# I don't know how to do this in an effcient way
+
 BASE_DIR = Path(__file__).resolve().parent
-APP_DIR = BASE_DIR.parent
-DATABASE_PATH = APP_DIR / "enrolment.db"
+AI_SERVICES_DIR = BASE_DIR.parent
+STUDENT_LOCATION_PATHS = AI_SERVICES_DIR.parent
+
+# you may have to reinitalise your database if you were having issues like I was lmao
+DB_S1_PATH = STUDENT_LOCATION_PATHS / "student-1" / "database" / "cm.db"
+
+
+# commented these out for now as they contain dummy data that will cause errors
+
+# DB_S2_PATH = STUDENT_LOCATION_PATHS / "student-2" / "database" / "databasename.db"
+# DB_S3_PATH = STUDENT_LOCATION_PATHS / "student-3" / "database" / "databasename.db"
+# DB_S4_PATH = STUDENT_LOCATION_PATHS / "student-4" / "database" / "databasename.db"
+
+# #student 5 will have to do this differently i think due to not having hte same set up
+# DB_S5_PATH = STUDENT_LOCATION_PATHS / "student-5" / "database" / "databasename.db"
 
 
 def _connect_db():
-    if not DATABASE_PATH.exists():
-        raise FileNotFoundError(f"Database not found: {DATABASE_PATH}")
-    conn = sqlite3.connect(DATABASE_PATH)
+    if not DB_S1_PATH.exists():
+        raise FileNotFoundError(f"Database not found: {DB_S1_PATH}")
+
+    # if not DB_S2_PATH.exists():
+    #     raise FileNotFoundError(f"Database not found: {DB_S2_PATH}")
+
+    # if not DB_S3_PATH.exists():
+    #     raise FileNotFoundError(f"Database not found: {DB_S3_PATH}")
+
+    # if not DB_S4_PATH.exists():
+    #     raise FileNotFoundError(f"Database not found: {DB_S4_PATH}")
+
+    # if not DB_S5_PATH.exists():
+    #     raise FileNotFoundError(f"Database not found: {DB_S5_PATH}")
+
+    
+    conn = sqlite3.connect(DB_S1_PATH)
+
+    # conn = sqlite3.connect(DB_S2_PATH)
+    # conn = sqlite3.connect(DB_S3_PATH)
+    # conn = sqlite3.connect(DB_S4_PATH)
+    # conn = sqlite3.connect(DB_S5_PATH)
+
     conn.row_factory = sqlite3.Row
     return conn
+
 
 
 
@@ -46,6 +84,13 @@ def read_ci_report(report_path: str = "../reports/report.json"):
 
 # For each database
 
+def debug_file_path():
+    print(BASE_DIR)
+    print(AI_SERVICES_DIR)
+    print(STUDENT_LOCATION_PATHS)
+    print(f"specific student:", DB_S1_PATH) 
+    print(_connect_db())
+
 
 def get_card_count():
     conn = _connect_db()
@@ -71,6 +116,7 @@ def get_card_per_user():
 
 
 if __name__ == "__main__":
+    debug_file_path()
     print(get_card_count())
     print(list_project_files(".."))
     print(read_ci_report("../reports/report.json"))
